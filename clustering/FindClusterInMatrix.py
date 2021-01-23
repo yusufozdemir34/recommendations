01c5cf58efe766e56f1473945849817580b13171
yozdemir <myusufozdemir@gmail.com>
2021-01-23 21:18:44 +0300

    there is clustering problem. so assign fixed value

--- clustering/FindClusterInMatrix.py
+++ clustering/FindClusterInMatrix.py
@@ -6,6 +6,8 @@ from collections import defaultdict
 
 def find_cluster_in_matrix(connected_point, n_user):
     connected_edge1, connected_edge2 = np.where(connected_point == 1)
+
+    # connected_edges = np.concatenate((connected_edge1, connected_edge2), axis=1)
     G = nx.Graph()
     user_nodes = np.arange(1, n_user)
     G.add_nodes_from(user_nodes)
@@ -13,6 +15,15 @@ def find_cluster_in_matrix(connected_point, n_user):
     print(connected_edge2)
     connected = [(int(connected_edge1), int(connected_edge2))]
     G.add_edges_from(connected)
+   # print("G is : ", G.nodes)
+    components = nx.connected_components(G)
+    # print("components is : ", components)
+    # for nodes in components:
+    #     print(nodes)
+    #     # for node in nodes:
+    #     #     print(node)
+    #
+    # print("merhaba")
 
 
 connected_edges = np.asmatrix('2  2  0 ; 1  0  0; 0  0  0')
