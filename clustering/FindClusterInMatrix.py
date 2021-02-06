fixed average cluster

clustering/FindClusterInMatrix.py
@@ -23,21 +23,26 @@ def find_cluster_in_matrix(connected_point, n_cluster):
     connected = tuple(map(tuple, connected))
     G.add_edges_from(connected)
     components = nx.connected_components(G)
+    result = []
+    for nodes in components:
+        temp = []
+        for node in nodes:
+            temp.append(node)
+        result.append(temp)
 
-    result = get_result_from_component(components, n_cluster)
+    # result = get_result_from_component(components, n_cluster)
 
     return result, G, components
 
 
 def get_result_from_component(components, n_cluster):
-    result = np.zeros((n_cluster, n_cluster))
+    result = []
     i = 0
     j = 0
     for nodes in components:
-        temp = np.zeros(n_cluster)
+        temp = []
         for node in nodes:
-            temp[j] = node
-            j = j + 1
-        result[i] = temp
+            temp.append(node)
+        result.append(temp)
         i = i + 1
     return result

recommendsystem/RecommendationHelper.py
@@ -156,13 +156,14 @@ def norm(n_users, clustered_user, user, n_cluster):
 
 def set_one_for_max_avg_value_others_zero(delta_mat):
     max_matrix = delta_mat.max(0)
-    for_i_size = np.size(delta_mat, 1) - 1
+    avg_matrix = np.mean(max_matrix)
+    for_i_size = np.size(delta_mat, 1)
     for_y_size = np.size(delta_mat, 0)
 
     # delta_math ın içindeki en yüksek avg yi bul. sonra en yüksek avg ye bir de. diğerleri sıfırdır.
     for j in range(0, for_i_size):
         for i in range(0, for_y_size):
-            if delta_mat[i][j] == max_matrix[j]:
+            if delta_mat[i][j] == max_matrix[j] and max_matrix[j] > avg_matrix*0.92:
                 delta_mat[i][j] = 1
             else:
                 delta_mat[i][j] = 0
@@ -199,13 +200,19 @@ def cluster_means(utility, clusters):
 
 def cluster_mean_from_components(utility, components):
     cluster_avg = []
-    # calculate average of each line (user)
-    for nodes in components:
-        temp = []
-        for node in nodes:
-            temp.append(utility[node])
-        cluster_avg.append(np.mean(temp))
+    for i in range(0, len(utility)):
+        cluster_avg.append(np.mean(utility[i]))
+
     return cluster_avg
+    #
+    # cluster_avg = []
+    # # calculate average of each line (user)
+    # for nodes in components:
+    #     temp = []
+    #     for node in nodes:
+    #         temp.append(utility[node])
+    #     cluster_avg.append(np.mean(temp))
+    # return cluster_avg
 
 
 def get_prediction(utility, pcs_matrix, user, cluster_users):